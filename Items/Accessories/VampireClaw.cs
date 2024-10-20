c3dbe95e1efa757589d88b63d36f0a3f117239b2
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2024-10-19 22:45:18 -0300

    4.0 update balanceamentos

--- Items/Accessories/VampireClaw.cs
+++ Items/Accessories/VampireClaw.cs
@@ -5,13 +5,10 @@ using Terraria.GameContent.ItemDropRules;
 using Terraria.GameContent.Creative;
 using System;
 
-namespace lifestealaccessory.Items.Accessories
-{
-    internal class NPCLOOT : GlobalNPC
-    {
+namespace lifestealaccessory.Items.Accessories{
+    internal class NPCLOOT : GlobalNPC{
         // ADICIONAR LOOT DROP:
-        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
-        {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot){
             int itemID = ModContent.ItemType<Items.Accessories.VampireClaw>();
             if(npc.netID == 174) // Herpling (Crimson)                                      // 0.5% CHANCE (1/200)
                 npcLoot.Add(ItemDropRule.Common(itemID,200, 1, 1));
@@ -20,19 +17,15 @@ namespace lifestealaccessory.Items.Accessories
         }
     }
 
-    public class VampireClaw : ModItem
-    {
-        public static readonly float LifeStealPercentage = 0.04f; // 4% de roubo de vida
+    public class VampireClaw : ModItem{
+        public static readonly float LifeStealPercentage = 0.05f; // 5% de roubo de vida
         public static DateTime lastHeal = DateTime.Now; // tempo no último heal
 
-
-        public override void SetStaticDefaults()
-        {
+        public override void SetStaticDefaults(){
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
         }
 
-        public override void SetDefaults()
-        {
+        public override void SetDefaults(){
 			Item.width = 16;
             Item.height = 16;
             Item.accessory = true;
@@ -41,8 +34,7 @@ namespace lifestealaccessory.Items.Accessories
             // Preço médio de reforja de buff no NPC Goblin: Apróx 3x o preço do sellPrice.
         }
         
-        public override void UpdateAccessory(Player player, bool hideVisual)
-        {
+        public override void UpdateAccessory(Player player, bool hideVisual){
             LifeStealPlayer modPlayer = player.GetModPlayer<LifeStealPlayer>();
             modPlayer.HasLifeStealAccessory = true;
             player.GetAttackSpeed(DamageClass.Melee) += (!modPlayer.nearDeath())? 0.04f : 0.08f;
--- lifestealaccessory.cs
+++ lifestealaccessory.cs
@@ -7,73 +7,61 @@ using lifestealaccessory.Items.Accessories;
 using System;
 // using System.Collections.Generic;
 
-namespace lifestealaccessory
-{
-    public class LifeStealPlayer : ModPlayer
-    {
-        /*  Não será usada por enquanto...
-        private HashSet<int> blacklistedNPCs = new HashSet<int>
-        {
-            488 // Target Dummy
-        }; */
-
-        // FLAG
-        public bool HasLifeStealAccessory = false;
-        public readonly int healCooldown = 100; // em millisegundos
-        // public readonly int healLimit = 10;
-
-        public bool canLifeSteal(int npcID)
-        {       // Função para verificar se pode ou não roubar vida...
-                return (HasLifeStealAccessory && npcID != 488);
-        }                                  // Target Dummy (evitar exploit - não pode)
-        
-        public bool nearDeath()
-        {       // Verifica se a vida atual é <= 15% da vida máxima.
-            return (Main.LocalPlayer.statLife <= Main.LocalPlayer.statLifeMax2*0.15f);
-        }
+namespace lifestealaccessory{
+    public class LifeStealPlayer : ModPlayer{
+        public bool HasLifeStealAccessory = false; // FLAG
+        public readonly int healCooldown = 410;    // em millisegundos
 
         // Sobrescrever OnHitNPC p/ lifesteal...
-        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
-        {
-            DateTime currentTime = DateTime.Now;
-            
-            if((currentTime - VampireClaw.lastHeal).TotalMilliseconds < healCooldown)
-                return; // Não cura.
-            
-            VampireClaw.lastHeal = currentTime;
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone){
+            DateTime currentTime = DateTime.Now; // Momento atual
 
-            if(canLifeSteal(target.netID))
-            {
+            if(canLifeSteal(target.netID)){
                 // Calcula o roubo de vida com base no dano e %.
-                
                 float percentage = VampireClaw.LifeStealPercentage;
-                bool passive = nearDeath();
-                if(passive) percentage += 0.02f; // Adiciona +2% Life Steal
+                bool passive = nearDeath(); // Ver se a passiva será aplicada (vida <= 15%)
+                
+                if(passive){
+                    percentage += 0.04f; // Adiciona +4% Life Steal
+                    if((currentTime - VampireClaw.lastHeal).TotalMilliseconds < healCooldown/2)
+                        return; // Não cura.
+                }
+                else
+                    if((currentTime - VampireClaw.lastHeal).TotalMilliseconds < healCooldown)
+                        return; // Não cura.
                 
                 int lifeStealAmount = (int) (damageDone * percentage);
-                if(lifeStealAmount > 0) // HP do jogo é (int)
-                {
-                    if(!passive) // Vida > 15%...
-                    {                              // Limites de heal:
+
+                if(lifeStealAmount > 0){ // HP do jogo é (int)
+                    if(!passive){
+
                         if(!hit.Crit) // Normal
-                            lifeStealAmount = (lifeStealAmount > 5)? 5 : lifeStealAmount;
+                            lifeStealAmount = (lifeStealAmount > 3)? 3 : lifeStealAmount;
                         else          // Crítico
-                            lifeStealAmount = (lifeStealAmount > 6)? 6 : lifeStealAmount;
+                            lifeStealAmount = (lifeStealAmount > 4)? 4 : lifeStealAmount;
                     }
-                    else{ // Vida <= 15%  -  Passiva ativada... 
-                        lifeStealAmount = (lifeStealAmount > 8)? 8 : lifeStealAmount;
-                    }
+                    else // passiva ativada 
+                        lifeStealAmount = (lifeStealAmount > 7)? 7 : lifeStealAmount;
                     
                     // Aplica a cura baseada no dano.
                     Main.LocalPlayer.statLife += lifeStealAmount; // Adiciona HP.
                     Main.LocalPlayer.HealEffect(lifeStealAmount); // Efeito visual da quantidade curada.
+                    VampireClaw.lastHeal = currentTime;           // Atualiza momento do último heal
                 }
             }
         }
 
-        public override void ResetEffects()
-        {
-            HasLifeStealAccessory = false;
+        public bool canLifeSteal(int npcID){  // Função para verificar se pode ou não roubar vida...
+            return (HasLifeStealAccessory && npcID != 488);   // id 488 - Target Dummy -> não roubar vida
         }
+        
+        public bool nearDeath(){  // Verifica se a vida atual é <= 15% da vida máxima.
+            return (Main.LocalPlayer.statLife <= Main.LocalPlayer.statLifeMax2*0.15f);
+        }
+        
+        public override void ResetEffects(){ 
+            HasLifeStealAccessory = false;
+        } // Função do tmodloader, sem isso os efeitos persistem após desequipar o item
+        
     }
 }
